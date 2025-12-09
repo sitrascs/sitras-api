58227bdc48c809a78428c2b2eac62da4b987a985
sitrascs <sitrascs@gmail.com>
2025-12-09 10:18:10 +0700

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,9 +1,26 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   role: { type: String, enum: ["admin", "user"], default: "user" },
 });
+
+// ==== HASH PASSWORD SEBELUM SAVE ====
+userSchema.pre("save", async function (next) {
+  // Jika password tidak diubah → lanjut
+  if (!this.isModified("password")) return next();
+
+  // Hash password
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+// ==== METHOD UNTUK CEK PASSWORD ====
+userSchema.methods.comparePassword = async function (inputPassword) {
+  return await bcrypt.compare(inputPassword, this.password);
+};
 
 module.exports = mongoose.model("User", userSchema);
